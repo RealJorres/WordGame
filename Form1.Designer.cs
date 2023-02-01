7a77aeb378c51eb3fd5c6075957165edfc727f4e
Joshua Relatorres <relatorres_joshua@yahoo.com>
2023-02-01 14:11:40 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WordGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guessCoount = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(845, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.toolTip1.SetToolTip(this.button1, "Generate new word");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(12, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(909, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 144);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(73, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Guess";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.guessCoount);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(933, 31);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guess Count:";
+            // 
+            // guessCoount
+            // 
+            this.guessCoount.AutoSize = true;
+            this.guessCoount.Location = new System.Drawing.Point(89, 9);
+            this.guessCoount.Name = "guessCoount";
+            this.guessCoount.Size = new System.Drawing.Size(13, 13);
+            this.guessCoount.TabIndex = 1;
+            this.guessCoount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 172);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Game";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label guessCoount;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WordGame
+{
+    public partial class Form1 : Form
+    {
+        Timer time;
+        int t = 0;
+        int left = 1;
+        string word;
+        int guessCount;
+        List<char> wordChar = new List<char>();
+        List<string> Words = new List<string>();
+        List<TextBox> textboxes = new List<TextBox>();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadWords();
+        }
+
+        public void DynamicText()
+        {
+            TextBox t = new TextBox();
+            t.Top = (panel1.DisplayRectangle.Height/2) -15;
+            t.Left = left * 32;
+            t.Size = new Size(30, 10);
+            t.Font = new Font("Monterrat", 18);
+            t.ReadOnly = true;
+            left++;
+            panel1.Controls.Add(t);
+            textboxes.Add(t);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = button2.Enabled = true;
+            GenerateNewWord();
+        }
+
+        private void GenerateNewWord()
+        {
+            textboxes.Clear();
+            wordChar.Clear();
+            panel1.Controls.Clear();
+            Random rnd = new Random();
+            word = Words[rnd.Next(0, Words.Count)].TrimStart().ToLower();
+            int wl = word.Length;
+            int o = 0;
+            left = 1;
+            for (int i = 0; i < wl; i++)
+            {
+                DynamicText();
+            }
+            foreach (char ii in word)
+            {
+                wordChar.Add(ii);
+            }
+            foreach (char iii in wordChar)
+            {
+                if (iii == '\'')
+                {
+                    textboxes[o].Text = '\''.ToString();
+                }
+                else if (iii == '(')
+                {
+                    textboxes[o].Text = '('.ToString();
+                }
+                else if (iii == ')')
+                {
+                    textboxes[o].Text = ')'.ToString();
+                }
+                o++;
+            }
+            guessCount = word.Length / 2;
+            //MessageBox.Show(word);
+        }
+
+        public void LoadWords()
+        {
+            string path = "words.txt";
+            List<string> fs = File.ReadAllLines(path).ToList(); 
+            foreach (var i in fs)
+            {
+                Words.Add(i);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Guess();
+        }
+
+        private void Guess()
+        {
+            try
+            {
+                int o = 0;
+                char[] guesss = textBox1.Text.ToCharArray();
+                char guess = guesss[0];
+                foreach (char i in wordChar)
+                {
+                    if (i == guess)
+                    {
+                        textboxes[o].Text = i.ToString();
+                    }
+                    o++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Guess field is empty.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                Guess();
+            }
+            else if (e.KeyChar == (char)27)
+            {
+                DialogResult ans =  MessageBox.Show("Reveal the answer?", "", MessageBoxButtons.YesNo);
+                if (ans.ToString().ToLower() == "yes")
+                {
+                    time = new Timer();
+                    time.Interval = 100;
+                    time.Tick += time_Tick;
+                    int o = 0;
+                    foreach (char i in wordChar)
+                    {
+                        textboxes[o].Text = i.ToString();
+                        o++;
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("No changes.");
+                }
+                
+            }
+        }
+
+        void time_Tick(object sender, EventArgs e)
+        {
+            if (t == 5)
+            {
+                time.Stop();
+                GenerateNewWord();
+            }
+            else
+            {
+                t++;
+            }
+        }
+    }
+
+}
